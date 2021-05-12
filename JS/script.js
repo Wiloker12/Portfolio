12ec5465739fc551dd197c7a25febde3d8528896
Wiloker1 <53343254+Wiloker1@users.noreply.github.com>
2021-05-13 02:01:32 +0300

    'Portfolio-0.0.5'

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
 function burgerClick(event){
+    let menu = document.querySelector('nav');
     let burger = document.querySelector('.header__burger');
+    menu.classList.toggle('active');
     burger.classList.toggle('active');
 }
